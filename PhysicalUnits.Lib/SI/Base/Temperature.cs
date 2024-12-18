0fa506a08694751693ded4761cd35ca2cd192a14
Philipp Siewert <philipp@phsiewert.de>
2024-12-18 13:56:02 +0100

    Added Temperature

---
+++ PhysicalUnits.Lib/SI/Base/Temperature.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicalUnits.Lib.SI.Base;
+
+public class Temperature(decimal value, Temperature.Units unit, PhysicalQuantity.Prefix prefix = PhysicalQuantity.Prefix.None) : PhysicalQuantity(value, unit, prefix)
+{
+    public enum Units
+    {
+        Celsius,
+        Fahrenheit,
+        Kelvin
+    }
+
+    public override decimal GetValue(Prefix? targetPrefix = Prefix.None)
+    {
+        targetPrefix ??= CurrentPrefix;
+        return _value / GetPrefixMultiplier((Prefix)targetPrefix);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Argumentausnahmen korrekt instanziieren", Justification = "<Ausstehend>")]
+    public override decimal GetValueInUnit(Enum targetUnit, Prefix? targetPrefix = Prefix.None)
+    {
+        targetUnit ??= Unit;
+        targetPrefix ??= CurrentPrefix;
+
+        if (targetUnit is not Units)
+            throw new ArgumentException("Invalid target unit for Temperature", nameof(targetUnit));
+
+        decimal baseValue = Unit switch
+        {
+            Units.Celsius => _value,
+            Units.Fahrenheit => (_value - 32m) * 5m / 9m,
+            Units.Kelvin => _value - 273.15m,
+            _ => throw new ArgumentOutOfRangeException(nameof(Unit), Unit, null)
+        };
+
+        decimal targetBaseValue = targetUnit switch
+        {
+            Units.Celsius => baseValue,
+            Units.Fahrenheit => (baseValue * 9m / 5m) + 32m,
+            Units.Kelvin => baseValue + 273.15m,
+            _ => throw new ArgumentOutOfRangeException(nameof(targetUnit), targetUnit, null)
+        };
+
+        return targetBaseValue / GetPrefixMultiplier(CurrentPrefix) * GetPrefixMultiplier((Prefix)targetPrefix);
+    }
+
+    public override string PrintUnit()
+    {
+        return CurrentPrefix == Prefix.None ? Unit.ToString() : CurrentPrefix.ToString() + Unit.ToString().ToLower();
+    }
+
+    public override string PrintUnitAbbreviation()
+    {
+        var prefixAbbreviation = GetPrefixAbbreviation(CurrentPrefix);
+
+        return Unit switch
+        {
+            Units.Celsius => prefixAbbreviation + "°C",
+            Units.Fahrenheit => prefixAbbreviation + "°F",
+            Units.Kelvin => prefixAbbreviation + "K",
+            _ => throw new ArgumentOutOfRangeException(nameof(Unit), Unit, null)
+        };
+    }
+}
---
+++ PhysicalUnits.Test/SI/Base/Temperature_Class_UnitTests.cs
@@ -0,0 +1,96 @@
+﻿using PhysicalUnits.Lib.SI.Base;
+
+namespace PhysicalUnits.Test.SI.Base;
+
+public class Temperature_Class_UnitTests
+{
+    [Fact]
+    public void GetValue_ReturnsCorrectValue()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Celsius);
+        var result = temperature.GetValue();
+
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromCelsiusInKelvin()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Celsius);
+        var result = temperature.GetValueInUnit(Temperature.Units.Kelvin);
+
+        Assert.Equal(373.15m, result, 4);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromCelsiusInFahrenheit()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Celsius);
+        var result = temperature.GetValueInUnit(Temperature.Units.Fahrenheit);
+
+        Assert.Equal(212, result, 4);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromKelvinInCelsius()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Kelvin);
+        var result = temperature.GetValueInUnit(Temperature.Units.Celsius);
+
+        Assert.Equal(-173.15m, result, 4);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromKelvinInFahrenheit()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Kelvin);
+        var result = temperature.GetValueInUnit(Temperature.Units.Fahrenheit);
+
+        Assert.Equal(-279.67m, result, 4);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromFahrenheitInCelsius()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Fahrenheit);
+        var result = temperature.GetValueInUnit(Temperature.Units.Celsius);
+
+        Assert.Equal(37.7778m, result, 4);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValueFromFahrenheitInKelvin()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Fahrenheit);
+        var result = temperature.GetValueInUnit(Temperature.Units.Kelvin);
+
+        Assert.Equal(310.9278m, result, 4);
+    }
+
+    [Fact]
+    public void SetValueInUnit_SetsValueCorrectly()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Kelvin);
+        temperature.SetValueInUnit(1, Temperature.Units.Celsius);
+
+        Assert.Equal(1, temperature.GetValue());
+    }
+
+    [Fact]
+    public void PrintUnit_PrintsCorrectUnit()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Celsius);
+        var result = temperature.PrintUnit();
+
+        Assert.Equal("Celsius", result);
+    }
+
+    [Fact]
+    public void PrintUnitAbbreviation_PrintsCorrectAbbreviation()
+    {
+        var temperature = new Temperature(100, Temperature.Units.Kelvin);
+        var result = temperature.PrintUnitAbbreviation();
+
+        Assert.Equal("K", result);
+    }
+}
