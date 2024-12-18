54705e0d1c4c545bec96255c96eb7019899711af
Philipp Siewert <philipp@phsiewert.de>
2024-12-18 13:59:21 +0100

    Added AmountOfSubstance

---
+++ PhysicalUnits.Lib/SI/Base/AmountOfSubstance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicalUnits.Lib.SI.Base;
+
+public class AmountOfSubstance(decimal value, PhysicalQuantity.Prefix prefix = PhysicalQuantity.Prefix.None) : PhysicalQuantity(value, Units.Mole, prefix)
+{
+    public enum Units
+    {
+        Mole
+    }
+
+    public override decimal GetValue(Prefix? targetPrefix = Prefix.None)
+    {
+        targetPrefix ??= CurrentPrefix;
+        return _value / GetPrefixMultiplier((Prefix)targetPrefix);
+    }
+
+    public override decimal GetValueInUnit(Enum targetUnit, Prefix? targetPrefix = Prefix.None)
+    {
+        targetPrefix ??= CurrentPrefix;
+        return GetValue(targetPrefix);
+    }
+
+    public override string PrintUnit()
+    {
+        return CurrentPrefix == Prefix.None ? Unit.ToString() + "s" : CurrentPrefix.ToString() + Unit.ToString().ToLower() + "s";
+    }
+
+    public override string PrintUnitAbbreviation()
+    {
+        var prefixAbbreviation = GetPrefixAbbreviation(CurrentPrefix);
+
+        return Unit switch
+        {
+            Units.Mole => prefixAbbreviation + "mol",
+            _ => throw new ArgumentOutOfRangeException(nameof(Unit), Unit, null)
+        };
+    }
+}
---
+++ PhysicalUnits.Test/SI/Base/AmountOfSubstance_Class_UnitTests.cs
@@ -0,0 +1,51 @@
+﻿using PhysicalUnits.Lib.SI.Base;
+
+namespace PhysicalUnits.Test.SI.Base;
+
+public class AmountOfSubstance_Class_UnitTests
+{
+    [Fact]
+    public void GetValue_ReturnsCorrectValue()
+    {
+        var amount = new AmountOfSubstance(100);
+        var result = amount.GetValue();
+
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValue()
+    {
+        var amount = new AmountOfSubstance(100);
+        var result = amount.GetValueInUnit(AmountOfSubstance.Units.Mole);
+
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void SetValueInUnit_SetsValueCorrectly()
+    {
+        var amount = new AmountOfSubstance(100);
+        amount.SetValueInUnit(1, AmountOfSubstance.Units.Mole);
+
+        Assert.Equal(1, amount.GetValue());
+    }
+
+    [Fact]
+    public void PrintUnit_PrintsCorrectUnit()
+    {
+        var amount = new AmountOfSubstance(100);
+        var result = amount.PrintUnit();
+
+        Assert.Equal("Moles", result);
+    }
+
+    [Fact]
+    public void PrintUnitAbbreviation_PrintsCorrectAbbreviation()
+    {
+        var amount = new AmountOfSubstance(100);
+        var result = amount.PrintUnitAbbreviation();
+
+        Assert.Equal("mol", result);
+    }
+}
