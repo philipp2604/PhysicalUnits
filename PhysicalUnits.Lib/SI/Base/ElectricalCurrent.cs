b734634a7622647f375c3b0e969c0768ab949b5e
Philipp Siewert <philipp@phsiewert.de>
2024-12-18 13:43:57 +0100

    Added ElectricalCurrent

---
+++ PhysicalUnits.Lib/SI/Base/ElectricalCurrent.cs
@@ -0,0 +1,37 @@
+﻿namespace PhysicalUnits.Lib.SI.Base;
+
+public class ElectricalCurrent(decimal value, PhysicalQuantity.Prefix prefix = PhysicalQuantity.Prefix.None) : PhysicalQuantity(value, Units.Ampere, prefix)
+{
+    public enum Units
+    {
+        Ampere
+    }
+
+    public override decimal GetValue(Prefix? targetPrefix = Prefix.None)
+    {
+        targetPrefix ??= CurrentPrefix;
+        return _value / GetPrefixMultiplier((Prefix)targetPrefix);
+    }
+
+    public override decimal GetValueInUnit(Enum targetUnit, Prefix? targetPrefix = Prefix.None)
+    {
+        targetPrefix ??= CurrentPrefix;
+        return GetValue(targetPrefix);
+    }
+
+    public override string PrintUnit()
+    {
+        return CurrentPrefix == Prefix.None ? Unit.ToString() + "s" : CurrentPrefix.ToString() + Unit.ToString().ToLower() + "s";
+    }
+
+    public override string PrintUnitAbbreviation()
+    {
+        var prefixAbbreviation = GetPrefixAbbreviation(CurrentPrefix);
+
+        return Unit switch
+        {
+            Units.Ampere => prefixAbbreviation + "A",
+            _ => throw new ArgumentOutOfRangeException(nameof(Unit), Unit, null)
+        };
+    }
+}
---
+++ PhysicalUnits.Test/SI/Base/ElectricalCurrent_Class_UnitTests.cs
@@ -0,0 +1,51 @@
+﻿using PhysicalUnits.Lib.SI.Base;
+
+namespace PhysicalUnits.Test.SI.Base;
+
+public class ElectricalCurrent_Class_UnitTests
+{
+    [Fact]
+    public void GetValue_ReturnsCorrectValue()
+    {
+        var current = new ElectricalCurrent(100);
+        var result = current.GetValue();
+
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void GetValueInUnit_ReturnsCorrectValue()
+    {
+        var current = new ElectricalCurrent(100);
+        var result = current.GetValueInUnit(ElectricalCurrent.Units.Ampere);
+
+        Assert.Equal(100, result);
+    }
+
+    [Fact]
+    public void SetValueInUnit_SetsValueCorrectly()
+    {
+        var current = new ElectricalCurrent(100);
+        current.SetValueInUnit(1, ElectricalCurrent.Units.Ampere);
+
+        Assert.Equal(1, current.GetValue());
+    }
+
+    [Fact]
+    public void PrintUnit_PrintsCorrectUnit()
+    {
+        var current = new ElectricalCurrent(100);
+        var result = current.PrintUnit();
+
+        Assert.Equal("Amperes", result);
+    }
+
+    [Fact]
+    public void PrintUnitAbbreviation_PrintsCorrectAbbreviation()
+    {
+        var current = new ElectricalCurrent(100);
+        var result = current.PrintUnitAbbreviation();
+
+        Assert.Equal("A", result);
+    }
+}
